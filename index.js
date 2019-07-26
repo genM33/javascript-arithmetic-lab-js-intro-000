1380f499ac0ed26e828917ffccc33a2a2bea5bfb
Geno M <giantpowpow2@gmail.com>
2019-07-26 01:14:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,6 +27,11 @@ function makeInt(n) {
   return parseInt(n, 10);
 }
 
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
+
 console.log(inc(100));
 console.log(dec(100));
 console.log(makeInt('78.987 Hello World'));
+console.log(preserveDecimal('78.987 Hello World'));
